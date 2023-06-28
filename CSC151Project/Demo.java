d7b4ef184936f8348a31c78d645ab0b1b8205213
DaltDton <slambe11@email.cpcc.edu>
2023-06-28 17:07:38 -0400

    Create Demo.java
    Readded Demo, wasnt in the right folder

---
+++ CSC151Project/Demo.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Demo{
+
+   public static void main (String args[]) throws IOException {
+   
+      List<Policy> policies = new ArrayList<>();
+      
+      File file = new File("C:/Users/dalto/OneDrive/Documents/GitHubRepositories/CSC151Project/Project_Samuel_Lambert/PolicyInformation.txt");
+      Scanner inputFile = new Scanner(file);
+   
+      while (inputFile.hasNextLine()) {
+         String policyNumber = inputFile.nextLine();
+         String providerName = inputFile.nextLine();
+         String policyholderFirstName = inputFile.nextLine();
+         String policyholderLastName = inputFile.nextLine();
+         int policyholdersAge = inputFile.nextInt();
+         inputFile.nextLine();
+         String policyholdersSmokingStat = inputFile.nextLine();
+         double policyholdersHeight = inputFile.nextDouble();
+         inputFile.nextLine();
+         double policyholdersWeight = inputFile.nextDouble();
+         inputFile.nextLine();
+         
+         if (inputFile.hasNextLine()) {
+            inputFile.nextLine();
+         }
+            
+      
+         Policy newPolicy = new Policy(policyNumber, providerName, policyholderFirstName, policyholderLastName, policyholdersAge, policyholdersSmokingStat, policyholdersHeight, policyholdersWeight);
+      
+         policies.add(newPolicy);
+      }
+   
+      for (Policy newPolicy : policies) {
+      
+         System.out.println("Policy Number: " + newPolicy.getPolicyNumber());
+         System.out.println("Provider Name: " + newPolicy.getProviderName());
+         System.out.println("Policyholder’s First Name: " + newPolicy.getPolicyholderFirstName());
+         System.out.println("Policyholder’s Last Name: " + newPolicy.getPolicyholderLastName());
+         System.out.println("Policyholder’s Age: " + newPolicy.getPolicyholdersAge());
+         System.out.println("Policyholder’s Smoking Status: " + newPolicy.getPolicyholdersSmokingStat());
+         System.out.println("Policyholder’s Height: " + newPolicy.getPolicyholdersHeight());
+         System.out.println("Policyholder’s Weight: " + newPolicy.getPolicyholdersWeightt());
+         System.out.printf("Policyholder’s BMI: $%.2f%n",  newPolicy.BMICalc());
+         System.out.printf("Policy Price: $%.2f%n", newPolicy.calcualtePrice());
+         System.out.println();
+      }
+      
+      int smokerCounter = 0;
+      int nonSmokerCounter = 0;
+   
+      for (Policy newPolicy : policies) {
+         if (newPolicy.getPolicyholdersSmokingStat().equalsIgnoreCase("smoker")) {
+            smokerCounter++;
+         } else {
+            nonSmokerCounter++;
+         }
+      }
+        
+      System.out.println("The number of policies with a smoker is: " + smokerCounter);
+      System.out.println("The number of policies with a non-smoker is: " + nonSmokerCounter);
+   }
+}
